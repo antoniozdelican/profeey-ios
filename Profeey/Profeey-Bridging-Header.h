da538551662f62da3b14fc1f46a860e3ec779096
antoniozdelican <antonio.zdelican@gmail.com>
2017-02-14 10:56:07 +0100

    removed unused camera

--- Profeey/Profeey-Bridging-Header.h
+++ Profeey/Profeey-Bridging-Header.h
@@ -2,8 +2,8 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "LLSimpleCamera.h"
-#import "LLSimpleCamera+Helper.h"
+//#import "LLSimpleCamera.h"
+//#import "LLSimpleCamera+Helper.h"
 #import "UIImage+FixOrientation.h"
 
 //#import "CommonCrypto/CommonCrypto.h"
